bacdd94e25615891c820eda237baa8905f3be9c1
Jean-Baptiste Dusseaut <157962+BodySplash@users.noreply.github.com>
2022-02-01 16:56:43 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,11 +1,47 @@
-import * as utils from '../utils';
 
-describe('utils', () => {
-  describe('utils#getMessage()', () => {
-    it('should return a message', () => {
-      const message = utils.getMessage();
-      expect(message).toEqual('Hello worldddd!');
-    });
+import {input} from './input_day1';
+
+
+function count_increments_in_depths_measurements(depths_measurements: number[]) {
+  const [, result] = depths_measurements.reduce(([previous, count]:[undefined|number, number], element)=> {
+
+    if(typeof previous !== "undefined" && element> previous) {
+      return [element, count+1];
+    }
+    return [element, count];
+
+  }, [undefined, 0]);
+  return result;
+}
+
+
+describe('Sonar sweep', () => {
+
+  const sample = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263];
+
+
+  it('do stuff', () => {
+
+    const result = count_increments_in_depths_measurements(sample);
+
+    expect(result).toEqual(7);
+
   });
-});
 
+  it('do for real', () => {
+      const result = count_increments_in_depths_measurements(input);
+
+      expect(result).toEqual(10);
+  });
+
+});
